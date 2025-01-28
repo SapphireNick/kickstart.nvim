4029685d5217c374226fccaa768702cb06d46bb6
Nikita-Nick Funk <nikita.nick.funk@gmail.com>
2025-01-28 04:03:16 +0100

    Add signature help plugin and new remaps
    Signed-off-by: Nikita-Nick Funk <nikita.nick.funk@gmail.com>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -18,4 +18,18 @@ return {
       }
     end,
   },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'InsertEnter',
+    opts = {
+      bind = true,
+      hint_enable = false,
+      handler_opts = {
+        border = 'single',
+      },
+    },
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
 }
